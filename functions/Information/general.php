efa34494aedb743195f9500590af092f4d3ce0fc
Rafael <contatorafaelcecchin@gmail.com>
2021-08-10 01:07:32 -0300

    fix: corrigir nome do grupo de configurações gerais

--- functions/Information/general.php
+++ functions/Information/general.php
@@ -6,7 +6,7 @@
         <h1>General</h1>
         <form method="post" action="options.php">';
                 
-            settings_fields( 'home_general_settings' ); // settings group name
+            settings_fields( 'general_group_settings' ); // settings group name
             do_settings_sections( 'general' ); // just a page slug
             submit_button();
 
@@ -18,5 +18,5 @@
 
     function register_general_settings() {
         $general_section = new CustomSection('general_section_settings', '', 'general');
-        $general_section->create_custom_option('logo', 'Logo', 'image', 'general_group_settings');
+        
     }
