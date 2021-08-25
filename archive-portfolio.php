c50657350c03d4f97a6a6a88d09f22325df39f7b
Rafael <contatorafaelcecchin@gmail.com>
2021-08-25 20:10:53 -0300

    feat: apagar custom archive e custom single para o portfólio

--- archive-portfolio.php
+++
@@ -1,5 +0,0 @@
-<?php rccode_get_custom_header(); ?>
-
-<?php rccode_get_custom_archive(); ?> 
-
-<?php rccode_get_custom_footer(); ?>
--- single-portfolio.php
+++
@@ -1,5 +0,0 @@
-<?php rccode_get_custom_header(); ?>
-
-<?php rccode_get_custom_single(); ?> 
-
-<?php rccode_get_custom_footer(); ?>
